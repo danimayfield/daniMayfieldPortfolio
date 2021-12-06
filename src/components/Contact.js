8a0a5485e8af53a017ddb5c67bef227e4b75ba82
Dani <daniella.m.gagliardi@gmail.com>
2021-12-06 00:12:58 -0500

    added proper links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,8 +7,8 @@ function Contact() {
       <h2>Contact</h2>
       <a href="#">danimmayfield@gmail.com</a>
       <div className="iconContainer">
-        <div className="iconHolder"><a className="contactIcon" href="#"><FiLinkedin/></a></div>
-        <div className="iconHolder"><a className="contactIcon" href="#"><FiGithub/></a></div>
+        <div className="iconHolder"><a className="contactIcon" href="https://www.linkedin.com/in/daniella-mayfield-5a6a99222/"><FiLinkedin/></a></div>
+        <div className="iconHolder"><a className="contactIcon" href="https://github.com/danimayfield"><FiGithub/></a></div>
         
       </div>
       <p>Website developed by Dani Mayfield 2021</p>
